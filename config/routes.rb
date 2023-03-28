c3891fa2c2920873bb265eafc1c12dbe72e5882d
yamato-1205 <ysakata1205@gmail.com>
2023-03-28 13:45:49 +0900

    ちょっとした階層の修正”

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  namespace :api do
-    namespace :v1 do
-      post '/authentications', to: 'authentications#create'
-      get '/auth/failure', to: 'authentications#failure'
-      get '/auth/:provider/callback', to: 'omniauth_callbacks#google_oauth2'
-    end
-  end
+  post '/authentications', to: 'authentications#create'
+  get '/auth/failure', to: 'authentications#failure'
+  get '/auth/:provider/callback', to: 'omniauth_callbacks#google_oauth2'
 end
