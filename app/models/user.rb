d08382fd2a553231aea94144430c2b8eb442984a
YuzukiAritomo <yuzuki.aritomo@gmail.com>
2023-03-28 18:52:10 +0900

    add user model

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+# # frozen_string_literal: true
+#
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :rememberable, :omniauthable
+  include DeviseTokenAuth::Concerns::User
+end
