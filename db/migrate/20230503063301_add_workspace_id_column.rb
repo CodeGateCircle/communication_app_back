1457bb96ef9f089573efcf49baadb0d97376f43b
Liver <hawkyofkn5365@gmail.com>
2023-05-04 21:37:53 +0900

    微調整

--- db/migrate/20230503063301_add_workspace_id_column.rb
+++ db/migrate/20230503063301_add_workspace_id_column.rb
@@ -1,7 +1,6 @@
 class AddWorkspaceIdColumn < ActiveRecord::Migration[7.0]
   def change
     add_column :categories, :workspace_id, :integer
-    # todo: rails db:migration:reset でできているかの確認
     # add_reference :categories, :workspace, foreign_key: true
     add_foreign_key :categories, :workspaces, columns: :workspace_id
   end
