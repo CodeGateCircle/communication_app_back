13417c7f74bed0079efc8ed159418ae02107f0dd
yamato-1205 <ysakata1205@gmail.com>
2023-05-09 13:27:41 +0900

    関数をapplication_controllerで共通化

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 # application
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
+
+  # ユーザーがworkspaceに属しているかチェック
+  def belong_to_workspace?(workspace_id)
+    user = WorkspaceUser.find_by(workspace_id:, user_id: current_user.id)
+    user.nil?
+  end
 end
--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -70,10 +70,5 @@ class RoomsController < ApplicationController
   rescue ArgumentError
     false
   end
-
-  # ユーザーがworkspaceに属しているかチェック
-  def belong_to_workspace?(workspace_id)
-    user = WorkspaceUser.find_by(workspace_id:, user_id: current_user.id)
-    user.nil?
-  end
+  
 end
