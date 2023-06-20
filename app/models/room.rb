57cc321ed3fe54ead6b3f46401b7ffd9ea1216b0
YuzukiAritomo <yuzuki.aritomo@gmail.com>
2023-06-20 21:28:20 +0900

    add message model

--- app/models/room.rb
+++ app/models/room.rb
@@ -7,4 +7,5 @@ class Room < ApplicationRecord
   # 中間テーブル
   has_many :room_users, dependent: :destroy
   has_many :users, through: :room_users
+  has_many :messages
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,7 @@ class User < ActiveRecord::Base
 
   has_many :room_users, dependent: :destroy
   has_many :rooms, through: :room_users
+  has_many :messages
 
   def self.from_omniauth(auth)
     user = User.where(provider: auth.provider, uid: auth.uid).first
