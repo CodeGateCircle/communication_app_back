a399959a7d6245149fa9afaa8d6b81b8c2d64c98
YuzukiAritomo <yuzuki.aritomo@gmail.com>
2023-10-17 21:23:32 +0900

    Update user serializer and allowed request origins in development and production environments

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -3,6 +3,6 @@ class UserSerializer < ActiveModel::Serializer
   type :user
 
   attributes :id, :name, :email, :image, :created_at, :updated_at
-  
+
   has_many :workspaces, serializer: WorkspaceSerializer
 end
