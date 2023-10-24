63dc42991829472a969220562379eb2b82a1bb22
yamato <82579341+yamato-1205@users.noreply.github.com>
2023-10-24 20:30:50 +0900

    リアクションのcreateを双方向通信対応 (#44)
    * リアクションのcreateを双方向通信対応
    
    * controllerのreaction_createの削除
    
    * コメントアウトの削除
    
    * rubcopエラーの解消
    
    * reaction_create残しました
    
    q
    
    exit
    :q
    
    ---------
    
    Co-authored-by: yuzuki aritomo <59410309+yuzuki-aritomo@users.noreply.github.com>

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -15,4 +15,23 @@ class ChatChannel < ApplicationCable::Channel
     )
     ActionCable.server.broadcast("chat_channel_#{message.room.workspace.id}", message)
   end
+
+  def reaction_create(data)
+    reaction = Reaction.create!(
+      message_id: data['messageId'],
+      user_id: current_user.id,
+      name: data['name']
+    )
+    ActionCable.server.broadcast("chat_channel_#{reaction.message.room.workspace.id}", reaction)
+  end
+
+  def reaction_delete(data)
+    reaction = Reaction.find_by(
+      message_id: data['messageId'],
+      user_id: current_user.id,
+      name: data['name']
+    )
+    reaction.destroy!
+    ActionCable.server.broadcast("chat_channel_#{reaction.message.room.workspace.id}", reaction)
+  end
 end
