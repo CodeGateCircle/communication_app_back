a54ced8f87d41689a4801b6eee46ecc2c07a20bc
yamato-1205 <ysakata1205@gmail.com>
2023-11-16 18:45:01 +0900

    ページネーションの作成

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,13 +3,13 @@ class MessagesController < ApplicationController
   before_action :belong_to_room?, only: %i[index]
   def index
     params = index_params
-    messages = Room.find(params[:room_id]).messages
+    messages = Room.find(params[:room_id]).messages.order("created_at DESC").page(params[:page]).per(20)
     render status: 200, json: messages, each_serializer: MessageSerializer
   end
 
   private
 
   def index_params
-    params.permit(:room_id)
+    params.permit(:room_id, :page)
   end
 end
